d8d886c320b5bf08a675ed403956bbe8ad934888
Noname <107889691+callmewhatuwant@users.noreply.github.com>
2026-01-21 23:48:00 +0100

    Refactor manager options in main.go

--- cmd/main.go
+++ cmd/main.go
@@ -32,6 +32,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	securityv1alpha1 "github.com/callmewhatuwant/age-secret-operator/api/v1alpha1"
@@ -100,16 +101,15 @@ func main() {
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-	    Scheme:  scheme,
-	    Metrics: metricsOpts,			
-		},
-		HealthProbeBindAddress: probeAddr,
-
-		// Leader Election (Lease in coordination.k8s.io)
-		LeaderElection:          enableLeaderElection,
-		LeaderElectionID:        "age-secret-operator.age.io",
-		LeaderElectionNamespace: leaderNS,
+	    Scheme:                 scheme,
+	    Metrics:                metricsOpts,
+	    HealthProbeBindAddress: probeAddr,
+	
+	    LeaderElection:          enableLeaderElection,
+	    LeaderElectionID:        "age-secret-operator.age.io",
+	    LeaderElectionNamespace: leaderNS,
 	})
+
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
