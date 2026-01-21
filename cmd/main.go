0334133b08507bc1853a65cdafcab90bce6663a1
Noname <107889691+callmewhatuwant@users.noreply.github.com>
2026-01-22 00:27:32 +0100

    Disable ServiceLevelObjective webhook setup
    Comment out webhook setup code for ServiceLevelObjective.

--- cmd/main.go
+++ cmd/main.go
@@ -238,13 +238,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AgeSecret")
 		os.Exit(1)
 	}
+	
 	// nolint:goconst
-	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
-		if err := webhookv1alpha1.SetupServiceLevelObjectiveWebhookWithManager(mgr); err != nil {
-			setupLog.Error(err, "unable to create webhook", "webhook", "ServiceLevelObjective")
-			os.Exit(1)
-		}
-	}
+	// if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		// if err := webhookv1alpha1.SetupServiceLevelObjectiveWebhookWithManager(mgr); err != nil {
+			// setupLog.Error(err, "unable to create webhook", "webhook", "ServiceLevelObjective")
+			// os.Exit(1)
+		// }
+	// }
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
