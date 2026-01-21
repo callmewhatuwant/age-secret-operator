86534b7c73f0a26452da857dc45d931ff4c49d59
Noname <107889691+callmewhatuwant@users.noreply.github.com>
2026-01-22 00:26:44 +0100

    Comment out webhook functionality in main.go
    Commented out webhook-related code and variables.

--- cmd/main.go
+++ cmd/main.go
@@ -29,12 +29,13 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	// for webhooks
+	// "sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	securityv1alpha1 "github.com/callmewhatuwant/age-secret-operator/api/v1alpha1"
 	"github.com/callmewhatuwant/age-secret-operator/internal/controller"
@@ -57,7 +58,9 @@ func main() {
 	var (
 		metricsAddr                                      string
 		metricsCertPath, metricsCertName, metricsCertKey string
-		secureMetrics                                    bool
+		secureMetrics  									 bool
+		// webhooks
+		// webhookCertPath, webhookCertName, webhookCertKey string
 		enableHTTP2                                      bool
 		probeAddr                                        string
 		enableLeaderElection                             bool
@@ -72,19 +75,29 @@ func main() {
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.BoolVar(&secureMetrics, "metrics-secure", true, "Serve metrics over HTTPS.")
+
+	// webhooks
+	//flag.StringVar(&webhookCertPath, "webhook-cert-path", "", "The directory that contains the webhook certificate.")
+	//flag.StringVar(&webhookCertName, "webhook-cert-name", "tls.crt", "The name of the webhook certificate file.")
+	//flag.StringVar(&webhookCertKey, "webhook-cert-key", "tls.key", "The name of the webhook key file.")
+	
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 	flag.StringVar(&leaderNS, "leader-election-namespace", "",
 		"Namespace for the leader election Lease (defaults to POD_NAMESPACE or age-system).")
+	
 	flag.StringVar(&keyNS, "key-namespace", "age-secrets", "Namespace containing AGE key Secrets.")
 	flag.StringVar(&keyLabelKey, "key-label-key", "app", "Label key for AGE key Secrets.")
 	flag.StringVar(&keyLabelVal, "key-label-val", "age-key", "Label value for AGE key Secrets.")
+	
 	flag.StringVar(&metricsCertPath, "metrics-cert-path", "",
 		"The directory that contains the metrics server certificate.")
 	flag.StringVar(&metricsCertName, "metrics-cert-name", "tls.crt", "The name of the metrics server certificate file.")
 	flag.StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "The name of the metrics server key file.")
+	
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
@@ -92,6 +105,8 @@ func main() {
 	}
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
+
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	// Resolve leader election namespace:
 	// explicit flag > POD_NAMESPACE > age-system
@@ -102,8 +117,7 @@ func main() {
 			leaderNS = "age-system"
 		}
 	}
-
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	
 
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
@@ -119,6 +133,40 @@ func main() {
 	if !enableHTTP2 {
 		tlsOpts = append(tlsOpts, disableHTTP2)
 	}
+
+	//// Initial webhook TLS options
+	// webhookTLSOpts := tlsOpts
+	// webhookServerOptions := webhook.Options{
+	// 	TLSOpts: webhookTLSOpts,
+	// }
+	//
+	// if len(webhookCertPath) > 0 {
+	// 	setupLog.Info(
+	// 		"Initializing webhook certificate watcher using provided certificates",
+	// 		"webhook-cert-path", webhookCertPath,
+	// 		"webhook-cert-name", webhookCertName,
+	// 		"webhook-cert-key", webhookCertKey,
+	// 	)
+	//
+	// 	webhookServerOptions.CertDir = webhookCertPath
+	// 	webhookServerOptions.CertName = webhookCertName
+	// 	webhookServerOptions.KeyName = webhookCertKey
+	// }
+	//
+	// webhookServer := webhook.NewServer(webhookServerOptions)
+
+
+	# if len(webhookCertPath) > 0 {
+	# 	setupLog.Info("Initializing webhook certificate watcher using provided certificates",
+	# 		"webhook-cert-path", webhookCertPath, "webhook-cert-name", webhookCertName, "webhook-cert-key", webhookCertKey)
+
+	# 	webhookServerOptions.CertDir = webhookCertPath
+	# 	webhookServerOptions.CertName = webhookCertName
+	# 	webhookServerOptions.KeyName = webhookCertKey
+	# }
+
+	# webhookServer := webhook.NewServer(webhookServerOptions)
+	
 
 	metricsServerOptions := metricsserver.Options{
 		BindAddress:   metricsAddr,
@@ -154,6 +202,7 @@ func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
+		// WebhookServer:          webhookServer,
 		HealthProbeBindAddress: probeAddr,
 
 		LeaderElection:          enableLeaderElection,
@@ -189,6 +238,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AgeSecret")
 		os.Exit(1)
 	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err := webhookv1alpha1.SetupServiceLevelObjectiveWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "ServiceLevelObjective")
+			os.Exit(1)
+		}
+	}
+	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
